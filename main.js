2c14e5d7e0b60efeee9934237cca542893d4549e
Micah Silverman <micah@afitnerd.com>
2021-09-23 14:39:32 -0400

    updated main to handle data loading

--- main.js
+++ main.js
@@ -16,20 +16,58 @@ for (const [paramName, paramValue] of urlParams) {
         paramValue.split(',').forEach(function (treatment) {
             treatments.push(treatment);
         })
+    } else if (paramName === 'fireEvents') {
+        fireEvents();
     } else {
         attributes[paramName] = paramValue;
     }
 }
 
+// If no key specified, default to ANONYMOUS
 if (!SplitConfig.core.key) {
     SplitConfig.core.key = 'ANONYMOUS';
 }
 
+// If no treatments specified, default to PacMan_RadarGhost
 if (!treatments.length) {
     treatments.push('PacMan_RadarGhost');
 }
 
 var splitClient = splitio(SplitConfig).client();
+
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function fireEvents() {
+    var origKey = SplitConfig.core.key;
+    var clients = [];
+    for (let i=0;i<10;i++) {
+        SplitConfig.core.key = `user_${(i+1)}`
+        clients.push({ 
+            user: SplitConfig.core.key, client: splitio(SplitConfig).client() 
+        });
+    }
+    const interval = setInterval(() => {
+        var clientInfo = clients[randomIntFromInterval(0, 9)];
+        clientInfo.client.ready().then(() => {
+            var treatmentResult = clientInfo.client.getTreatment(
+                'PacMan_RadarGhost', attributes
+            );
+            var ttl = (treatmentResult === 'on') ? 
+                randomIntFromInterval(3000, 10000) :
+                randomIntFromInterval(30000, 100000);
+            console.log(`ttl for user: ${clientInfo.user} is ${ttl} ` + 
+                `with treatment: ${treatmentResult}`);
+            localClient.track('user', 'PacMan_TTL', ttl);
+        });
+    }, 500);
+
+    setTimeout(() => {
+        clearInterval(interval);
+        SplitConfig.core.key = origKey;
+    }, 20000);
+}
 
 function handleTreatments() {
     var treatmentsResult = splitClient.getTreatments(treatments, attributes);
@@ -38,24 +76,24 @@ function handleTreatments() {
     );
 }
 
-splitClient.on(splitClient.Event.SDK_READY, function () {
-    handleTreatments();
+splitClient.ready().then(() => {
     console.log('Split is ready!');
-});
-
-splitClient.on(splitClient.Event.SDK_UPDATE, function () {
     handleTreatments();
-    console.log('The SDK has been updated!');
 });
 
-el.addEventListener('lifeLost', function(e) {
+splitClient.on(splitClient.Event.SDK_UPDATE, () => {
+    console.log('An streaming update has been received!');
+    handleTreatments();
+});
+
+el.addEventListener('lifeLost', (e) => {
     splitClient.track('user', 'PacMan_TTL', e.detail.pacman.ttl);
     console.log(`track PacMan_TTL with: ${e.detail.pacman.ttl}`);
 });
 
 if (Modernizr.canvas && Modernizr.localstorage &&
     Modernizr.audio && (Modernizr.audio.ogg || Modernizr.audio.mp3)) {
-    window.setTimeout(function () { PACMAN.init("./"); }, 0);
+    window.setTimeout(() => { PACMAN.init("./"); }, 0);
 } else {
     el.innerHTML = "Sorry, needs a decent browser<br /><small>" +
         "(firefox 3.6+, Chrome 4+, Opera 10+ and Safari 4+)</small>";
